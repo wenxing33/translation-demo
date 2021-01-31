d41e3d5c74bbd42873850a4e2132bad80ae86d84
Wenxing Wang <wenxing.wang@uk.ey.com>
2021-01-31 23:52:39 +0000

    feat: 3 languages

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component, Suspense } from 'react';
+import './App.css';
+import { useTranslation, withTranslation } from 'react-i18next';
+
+function HeaderComponent() {
+  const [t, i18n] = useTranslation('common');
+  return (
+    <div>
+      <h1>{t('welcome.title', { framework: 'React' })}</h1>
+      <button onClick={() => i18n.changeLanguage('de')}>de</button>
+      <button onClick={() => i18n.changeLanguage('en')}>en</button>
+      <button onClick={() => i18n.changeLanguage('zh')}>zh</button>
+    </div>
+  );
+}
+
+class HighOrderComponent extends Component {
+  render() {
+    const { t } = this.props;
+    console.log(this.props);
+
+    return <h1>{t('welcome.title', { framework: 'React' })}</h1>;
+  }
+}
+const HighOrderComponentTranslated = withTranslation('common')(HighOrderComponent);
+
+function App() {
+  return (
+    <Suspense fallback="loading">
+      <div className="App">
+        <HeaderComponent />
+        <HighOrderComponentTranslated />
+      </div>
+    </Suspense>
+  );
+}
+
+export default App;
